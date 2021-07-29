4da39433c15aa8bdc3b292ec911e7ce32716d447
Sean Andrews <sandrews@cfa.harvard.edu>
2021-07-29 11:54:07 -0400

    mechanics of packing data for inference

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ import os, sys, importlib
 import numpy as np
 import const as const
 
-# General visibility dataset class
+# General visibility dataset object
 class dataset:
 
    def __init__(self, um, vm, vis, wgt, nu_TOPO, nu_LSRK, tstamp_ID):
@@ -24,61 +24,134 @@ class dataset:
         self.nstamps = len(np.unique(tstamp_ID))
 
 
-# Fitting class
-class fitdata:
 
-    def __init__(self, file_prefix, vra, vcensor):
 
-        # Load the configuration file
-        inp = importlib.import_module('mconfig_'+file_prefix)
+# Inference-specific dataset object
+class inf_dataset:
 
-        # Load the metadata dictionary
-        data_dict = np.load(inp.dataname+'.npy', allow_pickle=True).item()
-        self.nobs = data_dict['nobs']
-        self.chbin = inp.chbin
+    def __init__(self, uu, vv, v_LSRK, tstamp_ID, vis_bin, wgt_bin, 
+                 M_bin, invM_bin, lnL0):
 
-        # Loop through each EB
-        for i in range(self.nobs):
+        # spatial frequencies in lambda units
+        self.uu = uu
+        self.vv = vv
 
-            # load the data into a dataset object
-            d_ = np.load(inp.dataname+'_EB'+str(i)+'.npz')
-            idata = dataset(d_['um'], d_['vm'], d_['data'], d_['weights'],
-                            d_['nu_TOPO'], d_['nu_LSRK'], d_['tstamp_ID'])
+        # timestamps (indices)
+        self.tstamp = tstamp_ID
+        self.nstamps = len(np.unique(tstamp_ID))
 
-            # convert the LSRK frequency grid to a velocity grid
-            v_LSRK = const.c_ * (1 - idata.nu_LSRK / inp.restfreq)
+        # **unbinned** LSRK velocity grid, model input velocities
+        self.v_LSRK = v_LSRK
+        self.v_calc = v_LSRK[np.int(self.nstamps / 2),:]
 
-            # fix direction of desired velocity bounds, based on data format
-            dvi, dvra = np.diff(v_LSRK, axis=1), np.diff(vra)
-            if np.logical_or(np.logical_and(np.all(dvi<0), np.all(dvra>0)),
-                             np.logical_and(np.all(dvi>0), np.all(dvra<0))): 
-                vra = vra[::-1]
-            sgn_v = np.sign(np.diff(vra)[0])
+        # **binned** data visibilities, weights
+        self.vis = vis_bin
+        self.wgt = wgt_bin
+        self.npol, self.nchan, self.nvis = self.vis.shape
+        self.chbin = np.int(len(self.v_calc) / self.vis.shape[1])
 
-            # find where to clip to lie within the desired velocity bounds
-            midstamp = np.int(idata.nstamps/2)
-            ixl = np.abs(v_LSRK[midstamp,:] - vra[0]).argmin()
-            ixh = np.abs(v_LSRK[midstamp,:] - vra[1]).argmin()
+        # likelihood-related quantities
+        self.cov = M_bin
+        self.inv_cov = invM_bin
+        self.lnL0 = lnL0
 
-            # reconcile channel set to be evenly divisible by binning factor
-            if ((ixh - ixl + (ixh - ixl) % inp.chbin[i]) < idata.nchan):
-                for j in range((ixh - ixl) % inp.chbin[i]):
-                    if not (ixh == idata.nchan-1):
-                        ixh += 1
-                    elif not (ixl == 0):
-                        ixl -= 1
+
+
+
+# Data parsing to generate inputs for likelihood function
+def fitdata(file_prefix, vra, vcensor):
+
+    # Load the configuration file
+    inp = importlib.import_module('mconfig_'+file_prefix)
+
+    # Load the metadata and initialize the output dictionary
+    data_dict = np.load(inp.dataname+'.npy', allow_pickle=True).item()
+    nobs = data_dict['nobs']
+    out_dict = {'nobs': nobs}
+
+    # Loop through each EB
+    for i in range(nobs):
+
+        # load the data into a dataset object
+        d_ = np.load(inp.dataname+'_EB'+str(i)+'.npz')
+        idata = dataset(d_['um'], d_['vm'], d_['data'], d_['weights'],
+                        d_['nu_TOPO'], d_['nu_LSRK'], d_['tstamp_ID'])
+
+        # if necessary, distribute weights across spectrum
+        if not idata.wgt.shape == idata.vis.shape:
+            idata.wgt = np.tile(idata.wgt, (idata.nchan, 1, 1))
+            idata.wgt = np.rollaxis(idata.wgt, 1, 0)
+
+        # convert the LSRK frequency grid to a velocity grid
+        v_LSRK = const.c_ * (1 - idata.nu_LSRK / inp.restfreq)
+
+        # fix direction of desired velocity bounds, based on data format
+        dvi, dvra = np.diff(v_LSRK, axis=1), np.diff(vra)
+        if np.logical_or(np.logical_and(np.all(dvi<0), np.all(dvra>0)),
+                         np.logical_and(np.all(dvi>0), np.all(dvra<0))): 
+            vra = vra[::-1]
+        sgn_v = np.sign(np.diff(vra)[0])
+
+        # find where to clip to lie within the desired velocity bounds
+        midstamp = np.int(idata.nstamps / 2)
+        ixl = np.abs(v_LSRK[midstamp,:] - vra[0]).argmin()
+        ixh = np.abs(v_LSRK[midstamp,:] - vra[1]).argmin()
+
+        # reconcile channel set to be evenly divisible by binning factor
+        if ((ixh - ixl + (ixh - ixl) % inp.chbin[i]) < idata.nchan):
+            for j in range((ixh - ixl) % inp.chbin[i]):
+                if not (ixh == idata.nchan-1):
+                    ixh += 1
+                elif not (ixl == 0):
+                    ixl -= 1
+                else:
+                    if j % 2 == 0: 
+                        ixh -= 1
                     else:
-                        if j % 2 == 0: 
-                            ixh -= 1
-                        else:
-                            ixl += 1
+                        ixl += 1
 
-            # clip the data
-            v_LSRK = v_LSRK[:,ixl:ixh]
-            idata.nu_LSRK = idata.nu_LSRK[:,ixl:ixh]
-            idata.nu_TOPO = idata.nu_TOPO[ixl:ixh]
-            idata.nchan = len(idata.nu_TOPO)
-            idata.vis = idata.vis[:,ixl:ixh,:]
-           
-                
-            sys.exit() 
+        # clip the data to cover only the frequencies of interest
+        iv_LSRK = v_LSRK[:,ixl:ixh]
+        inu_LSRK = idata.nu_LSRK[:,ixl:ixh]
+        inchan = inu_LSRK.shape[1]
+        ivis = idata.vis[:,ixl:ixh,:]
+        iwgt = idata.wgt[:,ixl:ixh,:]
+
+        # convert spatial frequencies to lambda units
+        iu = idata.um * np.mean(inu_LSRK[np.int(idata.nstamps/2),:]) / const.c_
+        iv = idata.vm * np.mean(inu_LSRK[np.int(idata.nstamps/2),:]) / const.c_
+
+        # spectral binning
+        bnchan = np.int(inchan / inp.chbin[i])
+        wt = iwgt.reshape((idata.npol, -1, inp.chbin[i], idata.nvis))
+        bvis = np.average(ivis.reshape((idata.npol, -1, inp.chbin[i], 
+                                        idata.nvis)), weights=wt, axis=2)
+        bwgt = np.sum(wt, axis=2)
+          
+        # pre-calculate the spectral covariance matrix and its inverse
+        if inp.chbin[i] == 2:
+            di, odi = (5./16), (3./32)
+        elif inp.chbin[i] == 3:
+            di, odi = (1./4), (1./24)
+        elif inp.chbin[i] == 4:
+            di, odi = (13./64), (3./128)
+        else:
+            di, odi = 1, 0      # this is wrong, but maybe useful to test
+        scov = di * np.eye(bnchan) + \
+               odi * (np.eye(bnchan, k=-1) + np.eye(bnchan, k=1))
+        scov_inv = np.linalg.inv(scov)
+
+        # pre-calculate the log-likelihood normalization
+        dterm = np.empty((idata.npol, idata.nvis))
+        for ii in range(idata.nvis):
+            for jj in range(idata.npol):
+                sgn, lndet = np.linalg.slogdet(scov / bwgt[jj,:,ii])
+                dterm[jj,ii] = sgn * lndet
+        lnL0 = -0.5*(np.prod(bvis.shape) * np.log(2*np.pi) + np.sum(dterm))
+
+        # package the data and add to the output dictionary
+        out_dict[str(i)] = inf_dataset(iu, iv, iv_LSRK, idata.tstamp, 
+                                       bvis, bwgt, scov, scov_inv, lnL0)
+
+    # return the output dictionary
+    return out_dict

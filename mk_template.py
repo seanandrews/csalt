8f050c173af48e72a0de859924beea33851d638a
Richard Teague <richard.d.teague@cfa.harvard.edu>
2021-08-11 16:14:32 -0400

    formatting of mk_template

--- mk_template.py
+++ mk_template.py
@@ -1,33 +1,37 @@
 """
     mk_template.py
 
-    Usage: 
-        > python mk_template.py <template_name>
+    Usage:
+    > python mk_template.py <template_name>
 
     Settings are edited by the user in 'tconfig_<template_name>.py'.
 
     Outputs:
-	- see CASA_scripts/mock_obs.py
+    - see CASA_scripts/mock_obs.py
 """
-import os, sys, time, importlib
-import numpy as np
-import const as const
+import os
+import sys
+from importlib import import_module
 
+# Parse the template name.
+
+template_name = sys.argv[-1]
 
 # Load template inputs
-inp = importlib.import_module('tconfig_'+sys.argv[-1])
 
+inp = import_module('tconfig_{}'.format(template_name))
 
 # Set up template storage space (if necessary)
-if inp.template_dir[-1] != '/': inp.template_dir += '/'
-if not os.path.exists(inp.template_dir):
-    os.system('mkdir '+inp.template_dir)
-    os.system('mkdir '+inp.template_dir+'sims')
-elif not os.path.exists(inp.template_dir+'sims'):
-    os.system('mkdir '+inp.template_dir+'sims')
 
+inp.template_dir += '/' if inp.template_dir[-1] != '/' else ''
+if not os.path.exists(inp.template_dir):
+    os.system('mkdir {}'.format(inp.template_dir))
+if not os.path.exists(inp.template_dir+'sims'):
+    os.system('mkdir {}sims'.format(inp.template_dir))
 
 # Generate the template
+
 os.system('rm -rf CASA_logs/mock_obs_'+sys.argv[-1]+'.log')
-os.system('casa --nologger --logfile CASA_logs/mock_obs_'+sys.argv[-1]+\
-          '.log -c CASA_scripts/mock_obs.py '+sys.argv[-1])
+os.system('casa --nologger '
+          + '--logfile CASA_logs/mock_obs_{}.log '.format(template_name)
+          + '-c CASA_scripts/mock_obs.py {}'.format(template_name))
